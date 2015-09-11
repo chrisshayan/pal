27f8a2c66d9f5c86851096563a1288eca6ec77e0
duynk <ricky.ngk@gmail.com>
2015-09-11 15:49:59 +0700

    try with multiple card type list

--- android/Pal/app/src/main/java/vietnamworks/com/pal/components/RecentThreadListAdapter.java
+++ android/Pal/app/src/main/java/vietnamworks/com/pal/components/RecentThreadListAdapter.java
@@ -17,37 +17,72 @@ import vietnamworks.com.pal.utils.Common;
  */
 public class RecentThreadListAdapter extends RecyclerView.Adapter<RecentThreadListAdapter.ViewHolder> {
     Context context;
+    public enum ECardType {
+        SEND_ME, RECENT_THREAD
+    }
+
     public  RecentThreadListAdapter(Context context) {
         this.context = context;
     }
 
     @Override
     public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.recent_thread_card, parent, false);
-        return new ViewHolder(view);
+        if (viewType == ECardType.SEND_ME.ordinal()) {
+            return new ViewHolder_Sendme(LayoutInflater.from(parent.getContext()).inflate(R.layout.sendme, parent, false));
+        } else if (viewType == ECardType.RECENT_THREAD.ordinal()) {
+            return new ViewHolder_RecentThread(LayoutInflater.from(parent.getContext()).inflate(R.layout.recent_thread_card, parent, false));
+        }
+        return null;
     }
 
     @Override
     public int getItemCount() {
         int totalItem = AppModel.recentThreadData.getData().size();
-        return totalItem;
+        return totalItem + 1;
     }
 
     @Override
-    public void onBindViewHolder(final ViewHolder holder, final int position) {
-        RecentThread item = (RecentThread) AppModel.recentThreadData.getData().get(position);
+    public int getItemViewType(int position) {
+        if (position == 0) {
+            return ECardType.SEND_ME.ordinal();
+        } else {
+            return ECardType.RECENT_THREAD.ordinal();
+        }
+    }
 
-        holder.uiTitle.setText(item.title);
-        holder.uiCreatedDate.setText(Common.getDateString(item.createdDate));
+
+    @Override
+    public void onBindViewHolder(final ViewHolder holder, final int position) {
+        if (position == 0) { //header
+            ViewHolder_Sendme item = (ViewHolder_Sendme)holder;
+        } else {
+            RecentThread item = (RecentThread) AppModel.recentThreadData.getData().get(position-1);
+            ViewHolder_RecentThread view = (ViewHolder_RecentThread)holder;
+            view.uiTitle.setText(item.title);
+            view.uiCreatedDate.setText(Common.getDateString(item.createdDate));
+        }
     }
 
     public class ViewHolder extends RecyclerView.ViewHolder {
+        public ViewHolder(View view){
+            super(view);
+        }
+    }
+
+    class ViewHolder_RecentThread extends RecentThreadListAdapter.ViewHolder{
         protected TextView uiTitle;
         protected TextView uiCreatedDate;
-        public ViewHolder(View view) {
+        public ViewHolder_RecentThread(View view) {
             super(view);
             this.uiTitle = (TextView) view.findViewById(R.id.title);
             this.uiCreatedDate = (TextView) view.findViewById(R.id.createdDate);
+        }
+    }
+
+    class ViewHolder_Sendme extends RecentThreadListAdapter.ViewHolder{
+        public ViewHolder_Sendme(View view) {
+            super(view);
+
         }
     }
 }
