7323dc12f204d9bfd8b1ca5741a7f1aadda20e34
Nguyen Khanh Duy <ricky.ngk@gmail.com>
2015-09-15 09:39:07 +0700

    Custom launcher screen

--- android/Pal/app/src/main/java/vietnamworks/com/pal/LauncherActivity.java
+++ android/Pal/app/src/main/java/vietnamworks/com/pal/LauncherActivity.java
@@ -5,13 +5,22 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
 public class LauncherActivity extends AppCompatActivity {
+    public final static long DELAY_TIME = 3000L;
+
+    public static LauncherActivity instance = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_launcher);
 
-        Intent intent = new Intent(this, TutorialActivity.class);
-        startActivity(intent);
+        instance = this;
+        new android.os.Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(LauncherActivity.instance, TutorialActivity.class);
+                startActivity(intent);
+            }
+        }, DELAY_TIME);
     }
 }
