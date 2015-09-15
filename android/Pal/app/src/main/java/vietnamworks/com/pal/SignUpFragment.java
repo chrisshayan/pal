ab6c399a5773a1b09175e9ac953f38bfd04cbfdf
Nguyen Khanh Duy <ricky.ngk@gmail.com>
2015-09-15 15:01:59 +0700

    layout signup screen

--- android/Pal/app/src/main/java/vietnamworks/com/pal/SignUpFragment.java
+++ android/Pal/app/src/main/java/vietnamworks/com/pal/SignUpFragment.java
@@ -9,14 +9,14 @@ import android.view.ViewGroup;
 /**
  * A placeholder fragment containing a simple view.
  */
-public class AuthActivityFragment extends Fragment {
+public class SignUpFragment extends Fragment {
 
-    public AuthActivityFragment() {
+    public SignUpFragment() {
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_auth, container, false);
+        return inflater.inflate(R.layout.fragment_signup, container, false);
     }
 }
