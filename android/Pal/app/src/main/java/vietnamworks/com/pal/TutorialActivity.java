0e420f101f9647a60122f8e242c67b9e2734ca3f
Nguyen Khanh Duy <ricky.ngk@gmail.com>
2015-09-15 11:26:56 +0700

    add tutorial flow & indicator

--- android/Pal/app/src/main/java/vietnamworks/com/pal/TutorialActivity.java
+++ android/Pal/app/src/main/java/vietnamworks/com/pal/TutorialActivity.java
@@ -1,5 +1,7 @@
 package vietnamworks.com.pal;
 
+import android.graphics.drawable.Drawable;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
@@ -7,10 +9,13 @@ import android.support.v4.app.FragmentStatePagerAdapter;
 import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 public class TutorialActivity extends AppCompatActivity {
 
-    private static final int NUM_PAGES = 3;
+    private static TutorialActivity sInstance;
+    private static int sNumPages;
     private ViewPager mPager;
     private PagerAdapter mPagerAdapter;
 
@@ -19,9 +24,54 @@ public class TutorialActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tutorial);
 
+        sNumPages = getResources().getStringArray(R.array.tutor).length;
+
         mPager = (ViewPager) findViewById(R.id.pager);
         mPagerAdapter = new ScreenSlidePagerAdapter(getSupportFragmentManager());
         mPager.setAdapter(mPagerAdapter);
+
+        sInstance = this;
+
+        sInstance.SetPageIndex(0);
+        mPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+                sInstance.SetPageIndex(position);
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+            }
+        });
+    }
+
+    public void SetPageIndex(int index) {
+        LinearLayout indicators = (LinearLayout) findViewById(R.id.tutor_indicator_holder);
+        indicators.removeAllViewsInLayout();
+
+        Drawable circle;
+        Drawable filled_circle;
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            circle = (Drawable) getResources().getDrawable(R.drawable.shape_circle, this.getTheme());
+            filled_circle = (Drawable) getResources().getDrawable(R.drawable.shape_filled_circle, this.getTheme());
+        } else {
+            circle = (Drawable)getResources().getDrawable(R.drawable.shape_circle);
+            filled_circle = (Drawable)getResources().getDrawable(R.drawable.shape_filled_circle);
+        }
+
+        for (int i = 0; i < mPagerAdapter.getCount(); i++) {
+            ImageView img = new ImageView(this);
+            img.setImageDrawable(index != i?circle:filled_circle);
+            indicators.addView(img);
+
+            LinearLayout.LayoutParams lp = new LinearLayout.LayoutParams(30, 30);
+            lp.setMargins(10, 0, 10, 0);
+            img.setLayoutParams(lp);
+        }
     }
 
     private class ScreenSlidePagerAdapter extends FragmentStatePagerAdapter {
@@ -31,12 +81,18 @@ public class TutorialActivity extends AppCompatActivity {
 
         @Override
         public Fragment getItem(int position) {
-            return TutorialFragment.create(position);
+            return TutorialFragment.create(position, sNumPages);
         }
 
         @Override
         public int getCount() {
-            return NUM_PAGES;
+            return sNumPages;
         }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        sInstance = null;
     }
 }
--- android/Pal/app/src/main/java/vietnamworks/com/pal/TutorialFragment.java
+++ android/Pal/app/src/main/java/vietnamworks/com/pal/TutorialFragment.java
@@ -16,12 +16,15 @@ public class TutorialFragment extends Fragment {
     }
 
     public static final String ARG_PAGE = "page";
+    public static final String ARG_TOTAL_PAGE = "total_page";
     private int mPageNumber;
+    private int mTotalPage;
 
-    public static TutorialFragment create(int pageNumber) {
+    public static TutorialFragment create(int pageNumber, int totalPage) {
         TutorialFragment fragment = new TutorialFragment();
         Bundle args = new Bundle();
         args.putInt(ARG_PAGE, pageNumber);
+        args.putInt(ARG_TOTAL_PAGE, totalPage);
         fragment.setArguments(args);
         return fragment;
     }
@@ -30,6 +33,7 @@ public class TutorialFragment extends Fragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         mPageNumber = getArguments().getInt(ARG_PAGE);
+        mTotalPage  = getArguments().getInt(ARG_TOTAL_PAGE);
     }
 
     @Override
@@ -39,14 +43,17 @@ public class TutorialFragment extends Fragment {
         ViewGroup rootView = (ViewGroup) inflater
                 .inflate(R.layout.fragment_tutorial, container, false);
 
-        // Set the title view to show the page number.
-        ((TextView) rootView.findViewById(android.R.id.text1)).setText(
-                getString(R.string.title_template_step, mPageNumber + 1));
+        String str = getResources().getStringArray(R.array.tutor)[mPageNumber];
+        ((TextView) rootView.findViewById(R.id.tutorTextView)).setText(str);
 
         return rootView;
     }
 
     public int getPageNumber() {
         return mPageNumber;
+    }
+
+    public int getTotalPages() {
+        return mTotalPage;
     }
 }
