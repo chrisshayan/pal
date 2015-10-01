4dec1a175779accbb7527ea958b012db0da659c8
Nguyen Khanh Duy <ricky.ngk@gmail.com>
2015-10-01 17:43:58 +0700

    use ffmpeg for linux

--- file_server/modules/android_audio.js
+++ file_server/modules/android_audio.js
@@ -3,6 +3,11 @@ var sys = require('sys');
 var crypto = require('crypto');
 var exec = require('child_process').exec;
 
+var ffmpeg;
+if (/^linux/.test(process.platform)) {
+    ffmpeg = require('fluent-ffmpeg');
+}
+
 function saveFile(file) {
     var fileRootName = file.name.split('.').shift(),
         fileExtension = file.name.split('.').pop(),
@@ -29,10 +34,19 @@ module.exports = function (req, res) {
 		var ext = filename.substring(filename.lastIndexOf("."), filename.length);
 		if (ext === ".3gp") {
 			console.log("Uploading file ... " + filename);
-			fstream = fs.createWriteStream(__dirname + '/../uploads/' + filename);
+            var des_path = __dirname + '/../uploads/' + filename;
+			fstream = fs.createWriteStream(des_path);
 			file.pipe(fstream);
 			fstream.on('close', function () {
 				console.log("Done : " + filename);
+
+                if (/^linux/.test(process.platform)) {
+                    //convert .3gp to mp3
+                    var FfmpegCommand = require('fluent-ffmpeg');
+                    var command = new FfmpegCommand();
+                }
+                var command  = ffmpeg(des_path).format('mp3');
+                command.save(des_path.replace(".3gp", ".mp3"));
 				res.json({"url": HOST + filename});
 			});
 		}
@@ -41,43 +55,3 @@ module.exports = function (req, res) {
 		}
 	});
 }
-
-
-/*
-function(req, res){
-	console.log("Received file:\n" + JSON.stringify(req.files));
-
-	var photoDir = __dirname+"/photos/";
-	var thumbnailsDir = __dirname+"/photos/thumbnails/";
-	var photoName = req.files.source.name;
-
-	fs.rename(
-		req.files.source.path,
-		photoDir+photoName,
-		function(err){
-			if(err != null){
-				console.log(err)
-				res.send({error:"Server Writting No Good"});
-			} else {
-				im.resize(
-					{
-						srcData:fs.readFileSync(photoDir+photoName, 'binary'),
-						width:256
-					},
-					function(err, stdout, stderr){
-						if(err != null){
-							console.log('stdout : '+stdout)
-
-							res.send({error:"Resizeing No Good"});
-						} else {
-							//console.log('ELSE stdout : '+stdout)
-							fs.writeFileSync(thumbnailsDir+"thumb_"+photoName, stdout, 'binary');
-							res.send("Ok");
-						}
-					}
-				);
-			}
-		}
-	);
-});
-*/
