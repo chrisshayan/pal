23a270af79a202dc81beaeae2a08c4ec4986f934
Nguyen Khanh Duy <ricky.ngk@gmail.com>
2015-10-02 10:04:36 +0700

    use https server

--- android/Pal/app/src/main/java/vietnamworks/com/pal/utils/Config.java
+++ android/Pal/app/src/main/java/vietnamworks/com/pal/utils/Config.java
@@ -8,6 +8,6 @@ public interface Config {
     public final static String SampleRecorderFilename = "recording" + RecorderFileExt;
 
     //public final static String AudioServerBaseURL = "http://172.18.2.150:4040";
-    public final static String AudioServerBaseURL = "http://iamprogrammer.work:4040";
+    public final static String AudioServerBaseURL = "https://iamprogrammer.work:4040";
     public final static String AudioUploadURL = AudioServerBaseURL + "/post_audio_android";
 }
