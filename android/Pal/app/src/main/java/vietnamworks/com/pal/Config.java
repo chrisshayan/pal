2ae19060506f0c3ff40c86d5cf5360080c131130
Nguyen Khanh Duy <ricky.ngk@gmail.com>
2015-10-05 14:10:21 +0700

    web server not handle transfer file yet, use direct address

--- android/Pal/app/src/main/java/vietnamworks/com/pal/Config.java
+++ android/Pal/app/src/main/java/vietnamworks/com/pal/Config.java
@@ -7,8 +7,7 @@ public interface Config {
     public final static String RecorderFileExt = ".3gp";
     public final static String SampleRecorderFilename = "recording" + RecorderFileExt;
 
-    //public final static String AudioServerBaseURL = "http://172.18.2.150:4040";
-    public final static String AudioServerBaseURL = "http://pal_server.iamprogrammer.work";
+    public final static String AudioServerBaseURL = "http://iamprogrammer.work:4040";
     public final static String AudioUploadURL = AudioServerBaseURL + "/post_audio_android";
 
     public final static String ParseAppId = "WRcgKehX6zd2idIpSUj6GGmwtcMipq7Y0tXzwJ2s";
