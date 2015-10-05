1b3256d0355e1357cb356acf6c060e7113f1f5a6
Nguyen Khanh Duy <ricky.ngk@gmail.com>
2015-10-05 14:10:40 +0700

    remove unused code

--- android/Pal/app/src/main/java/vietnamworks/com/pal/services/BaseService.java
+++ android/Pal/app/src/main/java/vietnamworks/com/pal/services/BaseService.java
@@ -45,70 +45,16 @@ public class BaseService {
         final UploadRequest request = new UploadRequest(context,
                 "custom-upload-id",
                 server);
-
-    /*
-     * parameter-name: is the name of the parameter that will contain file's data.
-     * Pass "uploaded_file" if you're using the test PHP script
-     *
-     * custom-file-name.extension: is the file name seen by the server.
-     * E.g. value of $_FILES["uploaded_file"]["name"] of the test PHP script
-     */
         request.addFileToUpload(filepath,
                 "parameter-name",
                 server_file_name,
                 "content-type");
 
-        /*
-        //You can add your own custom headers
-        request.addHeader("your-custom-header", "your-custom-value");
-
-        //and parameters
-        request.addParameter("parameter-name", "parameter-value");
-
-        //If you want to add a parameter with multiple values, you can do the following:
-        request.addParameter("array-parameter-name", "value1");
-        request.addParameter("array-parameter-name", "value2");
-        request.addParameter("array-parameter-name", "valueN");
-
-        //or
-        String[] values = new String[] {"value1", "value2", "valueN"};
-        request.addArrayParameter("array-parameter-name", values);
-
-        //or
-        List<String> valuesList = new ArrayList<String>();
-        valuesList.add("value1");
-        valuesList.add("value2");
-        valuesList.add("valueN");
-        request.addArrayParameter("array-parameter-name", valuesList);
-
-        //configure the notification
-        request.setNotificationConfig(android.R.drawable.ic_menu_upload,
-                "notification title",
-                "upload in progress text",
-                "upload completed successfully text"
-                "upload error text",
-                false);
-
-        // set a custom user agent string for the upload request
-        // if you comment the following line, the system default user-agent will be used
-        request.setCustomUserAgent("UploadServiceDemo/1.0");
-
-        // set the intent to perform when the user taps on the upload notification.
-        // currently tested only with intents that launches an activity
-        // if you comment this line, no action will be performed when the user taps on the notification
-        request.setNotificationClickIntent(new Intent(context, YourActivity.class));
-
-        */
-
-        // set the maximum number of automatic upload retries on error
         request.setMaxRetries(2);
-
         try {
-            //Start upload service and display the notification
             UploadService.startUpload(request);
 
         } catch (Exception exc) {
-            //You will end up here only if you pass an incomplete UploadRequest
             Log.e("AndroidUploadService", exc.getLocalizedMessage(), exc);
         }
     }
