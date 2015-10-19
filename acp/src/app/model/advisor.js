f21f04315273be640f0c4ff7a8916c42d1fcd2a2
Nguyen Khanh Duy <ricky.ngk@gmail.com>
2015-10-19 16:14:27 +0700

    add new advisor

--- acp/src/app/model/advisor.js
+++ acp/src/app/model/advisor.js
@@ -1,11 +1,30 @@
-angular.module('inspinia').service('AdvisorService', function (firebaseHelper) {
+angular.module('inspinia').service('AdvisorService', function ($rootScope, firebaseHelper) {
     this.getAdvisorById = function(uid, callback) {
         firebaseHelper.getFireBaseInstance(["profiles_pub", uid]).once('value', function(snapshot) {
             var obj = snapshot.val();
-            obj.$id = snapshot.key();
-            if (callback) {
+            if (obj && callback) {
+                obj.$id = snapshot.key();
                 callback(obj);
             }
         })
+    };
+
+    this.updateAdvisor = function(advisor, onComplete) {
+        var advisor_id = advisor.get("$id");
+        if (advisor_id) {
+            firebaseHelper.getFireBaseInstance(["profiles_pub", advisor.get("$id")]).update(advisor.getProperty(), function(error) {
+                if (error) {
+                    if (onComplete) {onComplete(error);}
+                } else {
+                    firebaseHelper.getFireBaseInstance(["profiles", advisor.get("$id")]).update({
+                        pub_profile_last_update: Date.now()
+                    }, function(error) {
+                        if (onComplete) {onComplete(error);}
+                    });
+                }
+            });
+        } else {
+            if (onComplete) {onComplete("Oops, something wrong. Invalid advisor id found");}
+        }
     }
 });
--- acp/src/app/modules/advisors/advisors.js
+++ acp/src/app/modules/advisors/advisors.js
@@ -3,7 +3,6 @@ angular.module('inspinia').controller('AdvisorsCtrl', function ($scope, firebase
     $scope.checked = {};
     $scope.nChecked = 0;
     $scope.isBan = true;
-    $scope.search = "";
 
     $scope.onSelectAdvisor = function() {
         var tmp = 0;
@@ -48,55 +47,7 @@ angular.module('inspinia').controller('AdvisorsCtrl', function ($scope, firebase
     }
 
     $scope.onAddAdvisor = function() {
-        var email = $scope.addUserEmail;
-        var display_name = $scope.addUserDisplayName;
-        if (email && display_name) {
-            var password = md5(Date.now() + Math.random());
-            firebaseHelper.getFireBaseInstance().createUser({
-                email: email,
-                password: password
-            }, function(error, userData) {
-                if (error) {
-                    switch (error.code) {
-                        case "EMAIL_TAKEN":
-                            $rootScope.notifyError("The new user account cannot be created because the email is already in use.");
-                            break;
-                        case "INVALID_EMAIL":
-                            $rootScope.notifyError("The specified email is not a valid email.");
-                            break;
-                        default:
-                            $rootScope.notifyError("Error creating user: " + error);
-                    }
-                } else {
-                    firebaseHelper.getFireBaseInstance(["profiles", userData.uid]).set({role: 'advisor'}, function(error) {
-                        if (error) {
-                            $rootScope.notifyError("Fail to create user profile " + error);
-                            firebaseHelper.getFireBaseInstance().removeUser({email: email, password: password});
-                        } else {
-                            firebaseHelper.getFireBaseInstance(["profiles_pub", userData.uid]).set({
-                                display_name: display_name,
-                                email: email,
-                                created_date: Date.now()
-                            }, function(error) {
-                                if (error) {
-                                    $rootScope.notifyError("Fail to create user public profile " + error);
-                                    firebaseHelper.getFireBaseInstance().removeUser({email: email, password: password});
-                                } else {
-                                    $rootScope.notifySuccess("Successfully created user account");
-                                }
-                            });
-                        }
-                    });
-                }
-            });
-        }
-        return true;
-    }
-
-    $scope.onCancelAddAdvisor = function() {
-        $scope.addUserEmail = "";
-        $scope.addUserDisplayName = "";
-        $scope.addUserMode = false;
+        $scope.openAdvisorModal();
     }
 
     $scope.onResetPassword = function() {
@@ -134,10 +85,6 @@ angular.module('inspinia').controller('AdvisorsCtrl', function ($scope, firebase
             $rootScope.notifyError("Your access privileges do not allow you to perform this action");
             return;
         }
-
-        $scope.addUserMode = false;
-        $scope.addUserEmail = "";
-        $scope.addUserDisplayName = "";
 
         $scope.advisors = {};
 
@@ -187,40 +134,65 @@ angular.module('inspinia').controller('AdvisorsCtrl', function ($scope, firebase
                 }
             }
         });
-
-        modalInstance.result.then(function (data, isnew) {
-            var obj = new Advisor(data);
-            if (isnew) {
-                obj.doCreate(firebaseHelper.getUID());
-            } else {
-                obj.doModify(firebaseHelper.getUID());
-                firebaseHelper.getFireBaseInstance(["profiles_pub", obj.get("$id")]).update(obj.getProperty(), function(error) {
-                    if (error) {
-                        $rootScope.notifyError(error);
-                    } else {
-                        firebaseHelper.getFireBaseInstance(["profiles", obj.get("$id")]).update({
-                            pub_profile_last_update: Date.now()
-                        }, function(error) {
-                            $rootScope.notifySuccess();
-                        });
-                    }
-                });
-            }
-        });
     }
 });
 
-angular.module('inspinia').controller('AdvisorModalCtrl', function($scope, $modalInstance, item, cs) {
+angular.module('inspinia').controller('AdvisorModalCtrl', function($rootScope, $scope, $modalInstance, item, cs, firebaseHelper, AdvisorService) {
     $scope.advisor = cs.purify(item || {});
     $scope.isNewUser = true;
+    $scope.isProcessing = false;
     if ($scope.advisor.$id) {
         $scope.isNewUser = false;
     }
     $scope.onDone = function () {
-        $modalInstance.close(cs.purify($scope.advisor), $scope.isNewUser);
+        if (!$scope.isProcessing && $scope.advisor.email && $scope.advisor.display_name) {
+            $scope.isProcessing = true;
+            var obj = new Advisor(cs.purify($scope.advisor));
+            var email = obj.get('email');
+            if ($scope.isNewUser) {
+                var password = md5(Date.now() + Math.random());
+                firebaseHelper.getFireBaseInstance().createUser({
+                    email: email,
+                    password: password
+                }, function(error, userData) {
+                    if (error) {
+                        $rootScope.notifyError(error);
+                        $scope.isProcessing = false;
+                    } else {
+                        firebaseHelper.getFireBaseInstance(["profiles", userData.uid]).set({role: 'advisor'}, function(error) {
+                            if (error) {
+                                $scope.isProcessing = false;
+                                $rootScope.notifyError("Fail to create user profile " + error);
+                                firebaseHelper.getFireBaseInstance().removeUser({email: email, password: password}, function(){});
+                            } else {
+                                obj.set('$id', userData.uid);
+                                obj.doCreate(firebaseHelper.getUID());
+                                AdvisorService.updateAdvisor(obj, function(error) {
+                                    if (error) {
+                                        $rootScope.notifyError("Fail to create user public profile " + error);
+                                        firebaseHelper.getFireBaseInstance().removeUser({email: email, password: password}, function(){});
+                                    } else {
+                                        $rootScope.notifySuccess("Successfully created user account");
+                                        $modalInstance.close();
+                                    }
+                                    $scope.isProcessing = false;
+                                });
+                            }
+                        });
+                    }
+                });
+            } else {
+                obj.doModify(firebaseHelper.getUID());
+                AdvisorService.updateAdvisor(obj, function() {
+                    $scope.isProcessing = false;
+                    $modalInstance.close();
+                });
+            }
+        }
     };
 
     $scope.cancel = function () {
+        $scope.isProcessing = false;
         $modalInstance.dismiss('cancel');
     };
 });
