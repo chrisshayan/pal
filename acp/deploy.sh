3cd205220b80c7a9220af54badc093f455d03f34
Nguyen Khanh Duy <ricky.ngk@gmail.com>
2015-10-23 02:05:28 +0700

    refine deploy script

--- acp/deploy.sh
+++ acp/deploy.sh
@@ -1,11 +1,3 @@
 #!/bin/sh
-for entry in "dist"/* "dist"/**/* "dist"/**/**/* "dist"/**/**/**/*
-do
-    folder=$(printf '%s' "$entry" | sed 's/dist//')
-    if [ ! -d "$entry" ]; then
-        scp -i id_rsa_iamprogrammer_work $entry admin@iamprogrammer.work:/usr/share/nginx/html/pal/$folder
-    else
-        ssh -i id_rsa_iamprogrammer_work admin@iamprogrammer.work "mkdir -p /usr/share/nginx/html/pal/$folder"
-    fi
-done
-open http://iamprogrammer.work/pal
+scp -i id_rsa_iamprogrammer_work -r dist admin@iamprogrammer.work:~/deploy/pal
+ssh -i id_rsa_iamprogrammer_work -t admin@iamprogrammer.work "sudo ln -s ~/deploy/pal/dist/ /usr/share/nginx/html/pal"
