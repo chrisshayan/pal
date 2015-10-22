13d09193c779207c02075c744709d0a102645f72
Nguyen Khanh Duy <ricky.ngk@gmail.com>
2015-10-22 19:42:55 +0700

    deploy script

---
+++ acp/deploy.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+for entry in "dist"/* "dist"/**/* "dist"/**/**/* "dist"/**/**/**/*
+do
+    folder=$(printf '%s' "$entry" | sed 's/dist//')
+    if [ ! -d "$entry" ]; then
+        scp -i id_rsa_iamprogrammer_work $entry admin@iamprogrammer.work:/usr/share/nginx/html/pal/$folder
+    else
+        ssh -i id_rsa_iamprogrammer_work admin@iamprogrammer.work "mkdir -p /usr/share/nginx/html/pal/$folder"
+    fi
+done
