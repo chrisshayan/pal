1fdf9239327ba7c41a86c5c9975f4608da871a4a
Nguyen Khanh Duy <ricky.ngk@gmail.com>
2015-10-26 18:41:24 +0700

    add auth fragments

--- android/pal2/app/src/main/java/vietnamworks/com/pal/activities/AuthActivity.java
+++ android/pal2/app/src/main/java/vietnamworks/com/pal/activities/AuthActivity.java
@@ -1,5 +1,6 @@
 package vietnamworks.com.pal.activities;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
@@ -11,7 +12,12 @@ import vietnamworks.com.pal.fragments.BaseFragment;
  * Created by duynk on 10/26/15.
  */
 public class AuthActivity extends BaseActivity {
-    BaseFragment loginFragment, registerFragment;
+    BaseFragment loginFragment, registerFragment, registerSuccessFragment;
+
+    final static int STATE_LOGIN = 0;
+    final static int STATE_REGISTER = 1;
+    final static int STATE_REGISTER_SUCCESS = 2;
+    int state;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,7 +28,49 @@ public class AuthActivity extends BaseActivity {
 
         loginFragment = (BaseFragment)getSupportFragmentManager().findFragmentById(R.id.fragment_login);
         registerFragment = (BaseFragment)getSupportFragmentManager().findFragmentById(R.id.fragment_register);
+        registerSuccessFragment = (BaseFragment)getSupportFragmentManager().findFragmentById(R.id.fragment_register_success);
 
-        registerFragment.getView().setVisibility(View.INVISIBLE);
+        setState(STATE_LOGIN);
     }
+
+    public void setState(final int _state) {
+        this.state = _state;
+        setTimeout(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    loginFragment.getView().setVisibility(state == STATE_LOGIN ? View.VISIBLE : View.INVISIBLE);
+                    registerFragment.getView().setVisibility(state == STATE_REGISTER ? View.VISIBLE : View.INVISIBLE);
+                    registerSuccessFragment.getView().setVisibility(state == STATE_REGISTER_SUCCESS ? View.VISIBLE : View.INVISIBLE);
+                } catch (Exception E) {
+                    E.printStackTrace();
+                }
+            }
+        });
+    }
+
+    public void onOpenRequestInvite(View v) {
+        setState(STATE_REGISTER);
+    }
+
+    public void onExecuteRequestInvite(View v) {
+        setState(STATE_REGISTER_SUCCESS);
+    }
+
+    public void onOpenLogin(View v) {
+        setState(STATE_LOGIN);
+    }
+
+    public  void onCloseRegisterSuccessDialog(View v) {
+        setState(STATE_LOGIN);
+    }
+
+    public void onShare(View v) {
+        Intent sendIntent = new Intent();
+        sendIntent.setAction(Intent.ACTION_SEND);
+        sendIntent.putExtra(Intent.EXTRA_TEXT, getString(R.string.share_text));
+        sendIntent.setType("text/plain");
+        startActivity(Intent.createChooser(sendIntent, getResources().getText(R.string.send_to)));
+    }
+
 }
