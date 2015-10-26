7d8befe1b687af2298dba3253fe51b502d182a43
Nguyen Khanh Duy <ricky.ngk@gmail.com>
2015-10-26 09:35:05 +0700

    init new app version

---
+++ android/pal2/app/src/main/java/vietnamworks/com/pal/MainActivity.java
@@ -0,0 +1,18 @@
+package vietnamworks.com.pal;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.WindowManager;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        setContentView(R.layout.activity_main);
+    }
+}
