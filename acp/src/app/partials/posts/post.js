c4156db6cac39b7c4d02786bbe447bf6d23cf4b4
Nguyen Khanh Duy <ricky.ngk@gmail.com>
2015-12-07 17:58:29 +0700

    post blinked when user pick a task

--- acp/src/app/partials/posts/post.js
+++ acp/src/app/partials/posts/post.js
@@ -7,6 +7,7 @@ angular.module('inspinia')
             ref: '='
         },
         controller: function($scope, firebaseHelper, $sce, $rootScope, cs, $http, parseHelper) {
+            $scope.isReady = false;
             $scope.formatTime = cs.formatTime;
             $scope.formatDate = cs.formatDate;
             $scope.formatDateTime = cs.formatDateTime;
@@ -15,6 +16,7 @@ angular.module('inspinia')
             $scope.user = firebaseHelper.getFireBaseInstance(["profiles_pub", $scope.data.created_by, "display_name"]).once('value', function(snapshot) {
                 $scope.user_display_name = snapshot.val() || "Unknown";
                 setTimeout(function(){
+                    $scope.isReady = true;
                     $scope.$digest();
                 }, 100);
             }, function() {});
