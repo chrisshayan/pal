41adacb19173a2ca454fb8552eae54669055b51a
Nguyen Khanh Duy <ricky.ngk@gmail.com>
2015-12-09 17:22:42 +0700

    missing woff2

--- acp/gulp/build.js
+++ acp/gulp/build.js
@@ -76,7 +76,7 @@ gulp.task('fonts', function () {
 });
 
 gulp.task('fontawesome', function () {
-    return gulp.src('bower_components/font-awesome/fonts/*.{eot,svg,ttf,woff}')
+    return gulp.src('bower_components/font-awesome/fonts/*.{eot,svg,ttf,woff,woff2}')
      .pipe(gulp.dest(paths.dist + '/fonts/'));
 });
 
